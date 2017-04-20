14845119fd996e7619283a208f85d2ab55d194d2
Luke Horsley <luke.horsley@offset1337.co.uk>
2017-04-21 00:22:02 +0100

    Included missing functionality from U24 and U48

--- src/primitives/u24.rs
+++ src/primitives/u24.rs
@@ -1,5 +1,6 @@
 use errors::*;
 use std::convert::TryFrom;
+use std::ops::Sub;
 
 pub const MAX: U24 = U24(0xff_ff_ff);
 pub const MIN: U24 = U24(0);
@@ -14,6 +15,20 @@ impl U24 {
 
     pub fn min_value() -> U24 {
         MIN
+    }
+}
+
+impl Sub for U24 {
+    type Output = U24;
+
+    fn sub(self, rhs: U24) -> Self::Output {
+        U24(self.0 - rhs.0)
+    }
+}
+
+impl From<u16> for U24 {
+    fn from(value: u16) -> U24 {
+        U24(value as u32)
     }
 }
 
@@ -34,3 +49,4 @@ impl TryFrom<u32> for U24 {
         }
     }
 }
+
--- src/primitives/u48.rs
+++ src/primitives/u48.rs
@@ -1,5 +1,6 @@
 use errors::*;
 use std::convert::TryFrom;
+use std::ops::Sub;
 
 pub const MAX: U48 = U48(0xff_ff_ff_ff_ff_ff);
 pub const MIN: U48 = U48(0);
@@ -14,6 +15,20 @@ impl U48 {
 
     pub fn min_value() -> U48 {
         MIN
+    }
+}
+
+impl Sub for U48 {
+    type Output = U48;
+
+    fn sub(self, rhs: U48) -> Self::Output {
+        U48(self.0 - rhs.0)
+    }
+}
+
+impl From<u32> for U48 {
+    fn from(value: u32) -> U48 {
+        U48(value as u64)
     }
 }
 
