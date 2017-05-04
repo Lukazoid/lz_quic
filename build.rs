ed7291ebfbad8e67fcf5fbc281efed5f39b367f3
Luke Horsley <luke.horsley@offset1337.co.uk>
2017-05-04 22:35:34 +0100

    Added a missing use of the result from vergen

--- build.rs
+++ build.rs
@@ -3,5 +3,6 @@ extern crate vergen;
 use vergen::{vergen, OutputFns};
 
 fn main() {
-    vergen(OutputFns::all());
+    vergen(OutputFns::all()).expect("there should be no issue generating the version info");
 }
+
