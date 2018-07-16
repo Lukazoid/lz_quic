95a83676f70965292ded7c9af498d0d6b15a17c8
Luke Horsley <luke.horsley@offset1337.co.uk>
2018-07-16 21:22:32 +0100

    The client_handshake example now writes full error details

--- examples/client_handshake.rs
+++ examples/client_handshake.rs
@@ -1,13 +1,37 @@
 extern crate env_logger;
+extern crate futures;
 extern crate lz_quic;
 extern crate tokio_core;
 
+use futures::Future;
+use lz_quic::Result as QuicResult;
 use lz_quic::{Client, ClientConfiguration, ServerId};
 use std::net::ToSocketAddrs;
 use tokio_core::reactor::Core;
 
 fn main() {
     env_logger::init();
+
+    if let Err(ref e) = run() {
+        use std::io::Write;
+        let stderr = &mut ::std::io::stderr();
+        let errmsg = "Error writing to stderr";
+
+        writeln!(stderr, "error: {}", e).expect(errmsg);
+
+        for e in e.iter().skip(1) {
+            writeln!(stderr, "caused by: {}", e).expect(errmsg);
+        }
+
+        if let Some(backtrace) = e.backtrace() {
+            writeln!(stderr, "backtrace: {:?}", backtrace).expect(errmsg);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> QuicResult<()> {
     let mut core = Core::new().expect("failed to create event loop");
     let server_id = ServerId::new("test.privateoctopus.com".to_owned(), 4433);
 
@@ -20,7 +44,7 @@ fn main() {
         server_id,
         ClientConfiguration::default(),
         &core.handle(),
-    );
+    ).map(|_| ());
 
-    core.run(when_connected).unwrap();
+    core.run(when_connected)
 }
